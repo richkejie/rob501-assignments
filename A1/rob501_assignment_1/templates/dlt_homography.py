7426c8ce8ea3a79d958067d529b2962a2fe37ea9
RichardMac <richkejie@gmail.com>
2025-09-15 18:24:06 -0400

    A1: dlt_homography code

--- A1/rob501_assignment_1/templates/dlt_homography.py
+++ A1/rob501_assignment_1/templates/dlt_homography.py
@@ -20,7 +20,23 @@ def dlt_homography(I1pts, I2pts):
     A  - 8x9 np.array of DLT matrix used to determine homography.
     """
     #--- FILL ME IN ---
+    A = np.zeros((8,9))
+    for i in range(0, I1pts.shape[1]):
+        x = I1pts[0,i]
+        y = I1pts[1,i]
+        u = I2pts[0,1]
+        v = I2pts[1,i]
 
+        A_i = np.array([
+            [ -x, -y, -1,  0,  0,  0, u*x, u*y, u],
+            [  0,  0,  0, -x, -y, -1, v*x, v*y, v],
+        ])
+
+        A[2*i:2*(i+1)] = A_i
+
+    H = null_space(A)
+    H = H.reshape(3,3)
+    H = 1/H[2,2] * H
     #------------------
 
     return H, A
