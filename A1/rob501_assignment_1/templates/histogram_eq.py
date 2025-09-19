9cbb393287cf88602aa416cf7145bca79f037bb2
RichardMac <richkejie@gmail.com>
2025-09-19 13:46:30 -0400

    completed algorithm for histogram_eq + comments

--- A1/rob501_assignment_1/templates/histogram_eq.py
+++ A1/rob501_assignment_1/templates/histogram_eq.py
@@ -22,6 +22,35 @@ def histogram_eq(I):
     if I.dtype != np.uint8:
         raise ValueError('Incorrect image format!')
 
+    # flatten img array for convenience
+    img = I.flatten() # flatten in row-major order
+    N = img.shape[0] # total number of pixels
+
+    # init J
+    J = np.empty(img.shape)
+
+    # hist calculation
+    nbins = 256 # 8bit pixels (0,255) range
+    rng = (0,255)
+    hist, _ = np.histogram(img, bins=nbins, range=rng)
+    
+    # calculate cumulative dist
+    PDF = hist / np.sum(hist) # normalized hist = pdf
+    CDF = np.cumsum(PDF)
+
+    # map CDF to (0,255) range
+    c = np.round(255*CDF)
+    # cast to uint8 for intensity
+    c = c.astype('uint8')
+
+    # f(i) = c(i)
+    # pixel i of new img J will have intensity of c[img[i]]
+    for i in range(N):
+        J[i] = c[img[i]]
+    
+    # reshape J
+    J = J.reshape(I.shape)
+
     #------------------
 
     return J
