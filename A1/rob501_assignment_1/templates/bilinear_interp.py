15af06ff388c3e2bd0448a08b22aa5c523c794b9
RichardMac <richkejie@gmail.com>
2025-09-24 10:11:15 -0400

    added linear interp for pixels on edge

--- A1/rob501_assignment_1/templates/bilinear_interp.py
+++ A1/rob501_assignment_1/templates/bilinear_interp.py
@@ -1,6 +1,10 @@
 import numpy as np
 from numpy.linalg import inv
 
+def linear_interp(x, x1, y1, x2, y2):
+    y = y1 + (x - x1) * (y2 - y1) / (x2 - x1)
+    return int(round(y))
+
 def bilinear_interp(I, pt):
     """
     Performs bilinear interpolation for a given image point.
@@ -31,6 +35,11 @@ def bilinear_interp(I, pt):
     Px = pt[0]
     Py = pt[1]
 
+    y_min = 0
+    y_max = I.shape[0] - 1
+    x_min = 0
+    x_max = I.shape[1] - 1
+
     # get 4 points (x,y) pixel coords from I
     x_1 = int(np.floor(Px))
     if (x_1 - Px) == 0: # Px int
@@ -43,6 +52,39 @@ def bilinear_interp(I, pt):
         y_1 -= 1
 
     y_2 = int(y_1 + 1)
+
+    # check if on edge and need to do linear interpolation
+    if y_1 <= y_min:
+        y_star = y_min
+    elif y_2 >= y_max:
+        y_star = y_max
+    else:
+        y_star = None
+    
+    if y_star != None:
+        return linear_interp(
+            x = Px[0],
+            x1 = x_1,
+            y1 = I[y_star][x_1],
+            x2 = x_2,
+            y2 = I[y_star][x_2]
+        )
+
+    if x_1 <= x_min:
+        x_star = x_min
+    elif x_2 >= x_max:
+        x_star = x_max
+    else:
+        x_star = None
+
+    if x_star != None:
+        return linear_interp(
+            x = Py[0],
+            x1 = y_1,
+            y1 = I[y_1][x_star],
+            x2 = y_2,
+            y2 = I[y_2][x_star]
+        )
 
     # 4 points are:
     # Q11(x_1, y_1)
