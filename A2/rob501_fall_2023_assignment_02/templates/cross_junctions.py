e8e97ed380f0cea08fd4500bf943135eab8e1279
RichardMac <richkejie@gmail.com>
2025-10-07 23:46:34 -0400

    adding initial A2 files

---
+++ A2/rob501_fall_2023_assignment_02/templates/cross_junctions.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy.linalg import inv, lstsq
+from scipy.linalg import null_space
+from scipy.ndimage.filters import *
+from matplotlib.path import Path
+
+# You may add support functions here, if desired.
+
+def cross_junctions(I, bpoly, Wpts):
+    """
+    Find cross-junctions in image with subpixel accuracy.
+
+    The function locates a series of cross-junction points on a planar 
+    calibration target, where the target is bounded in the image by the 
+    specified quadrilateral. The number of cross-junctions identified 
+    should be equal to the number of world points.
+
+    Note also that the world and image points must be in *correspondence*,
+    that is, the first world point should map to the first image point, etc.
+
+    Parameters:
+    -----------
+    I      - Single-band (greyscale) image as np.array (e.g., uint8, float).
+    bpoly  - 2x4 np.array, bounding polygon (clockwise from upper left).
+    Wpts   - 3xn np.array of world points (in 3D, on calibration target).
+
+    Returns:
+    --------
+    Ipts  - 2xn np.array of cross-junctions (x, y), relative to the upper
+            left corner of the target. The array must contain float64 values.
+    """
+    #--- FILL ME IN ---
+
+    # Code goes here...
+
+    #------------------
+
+    correct = isinstance(Ipts, np.ndarray) and \
+        Ipts.dtype == np.float64 and Ipts.shape[0] == 2
+
+    if not correct:
+        raise TypeError("Wrong type or size returned!")
+
+    return Ipts
---
+++ A2/rob501_fall_2023_assignment_02/templates/find_jacobian.py
@@ -0,0 +1,39 @@
+import numpy as np
+from numpy.linalg import inv
+
+def find_jacobian(K, Twc, Wpt):
+    """
+    Determine the Jacobian for NLS camera pose optimization.
+
+    The function computes the Jacobian of an image plane point with respect
+    to the current camera pose estimate, given a landmark point. The 
+    projection model is the simple pinhole model.
+
+    Note that the homogeneous transformation matrix provided defines the
+    transformation from the *camera frame* to the *world frame* (to 
+    project into the image, you would need to invert this matrix).
+
+    Parameters:
+    -----------
+    K    - 3x3 np.array, camera intrinsic calibration matrix.
+    Twc  - 4x4 np.array, homogenous pose matrix, current guess for camera pose. 
+    Wpt  - 3x1 world point on calibration target (one of n).
+
+    Returns:
+    --------
+    J  - 2x6 np.array, Jacobian matrix (columns are tx, ty, tz, r, p, q).
+         The Jacobian must contain float64 values.
+    """
+    #--- FILL ME IN ---
+ 
+    # Code goes here...
+
+    #------------------
+
+    correct = isinstance(J, np.ndarray) and \
+        J.dtype == np.float64 and J.shape == (2, 6)
+
+    if not correct:
+        raise TypeError("Wrong type or size returned!")
+
+    return J
