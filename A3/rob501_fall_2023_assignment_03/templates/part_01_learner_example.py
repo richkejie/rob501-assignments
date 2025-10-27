4520831d007cce6b2931f7c0b4a93c94bddf2cbc
RichardMac <richkejie@gmail.com>
2025-10-27 11:45:57 -0400

    adding stereo disparity score helper file; learner examples updated mode for compatibility

--- A3/rob501_fall_2023_assignment_03/templates/part_01_learner_example.py
+++ A3/rob501_fall_2023_assignment_03/templates/part_01_learner_example.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from imageio import imread
+from imageio.v2 import imread
 from stereo_disparity_score import stereo_disparity_score
 from stereo_disparity_fast import stereo_disparity_fast
 
 # Load the stereo images and ground truth.
-Il = imread("../images/cones/cones_image_02.png", as_gray = True)
-Ir = imread("../images/cones/cones_image_06.png", as_gray = True)
+# Il = imread("../images/cones/cones_image_02.png", as_gray = True)
+# Ir = imread("../images/cones/cones_image_06.png", as_gray = True)
+Il = imread("../images/cones/cones_image_02.png", mode='F')
+Ir = imread("../images/cones/cones_image_06.png", mode='F')
 
 # The cones and teddy datasets have ground truth that is 'stretched'
 # to fill the range of available intensities - here we divide to undo.
-It = imread("../images/cones/cones_disp_02.png",  as_gray = True)/4.0
+# It = imread("../images/cones/cones_disp_02.png",  as_gray = True)/4.0
+It = imread("../images/cones/cones_disp_02.png",  mode='F')/4.0
 
 # Load the appropriate bounding box.
 bbox = np.load("../data/cones_02_bounds.npy")
--- A3/rob501_fall_2023_assignment_03/templates/part_02_learner_example.py
+++ A3/rob501_fall_2023_assignment_03/templates/part_02_learner_example.py
@@ -5,12 +5,15 @@ from stereo_disparity_score import stereo_disparity_score
 from stereo_disparity_best import stereo_disparity_best
 
 # Load the stereo images and ground truth.
-Il = imread("../images/cones/cones_image_02.png", as_gray = True)
-Ir = imread("../images/cones/cones_image_06.png", as_gray = True)
+# Il = imread("../images/cones/cones_image_02.png", as_gray = True)
+# Ir = imread("../images/cones/cones_image_06.png", as_gray = True)
+Il = imread("../images/cones/cones_image_02.png", mode='F')
+Ir = imread("../images/cones/cones_image_06.png", mode='F')
 
 # The cones and teddy datasets have ground truth that is 'stretched'
 # to fill the range of available intensities - here we divide to undo.
-It = imread("../images/cones/cones_disp_02.png",  as_gray = True)/4.0
+# It = imread("../images/cones/cones_disp_02.png",  as_gray = True)/4.0
+It = imread("../images/cones/cones_disp_02.png",  mode='F')/4.0
 
 # Load the appropriate bounding box.
 bbox = np.load("../data/cones_02_bounds.npy")
