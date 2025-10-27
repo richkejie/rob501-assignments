a52e74b102e753975533cb5afa3909790842799f
RichardMac <richkejie@gmail.com>
2025-10-26 20:19:25 -0400

    adding initial lab3 files

---
+++ A3/rob501_fall_2023_assignment_03/templates/stereo_disparity_best.py
@@ -0,0 +1,48 @@
+import numpy as np
+from scipy.ndimage.filters import *
+
+def stereo_disparity_best(Il, Ir, bbox, maxd):
+    """
+    Best stereo correspondence algorithm.
+
+    This function computes a stereo disparity image from left stereo 
+    image Il and right stereo image Ir. Only disparity values within
+    the bounding box region (inclusive) are evaluated.
+
+    Parameters:
+    -----------
+    Il    - Left stereo image, m x n pixel np.array, greyscale.
+    Ir    - Right stereo image, m x n pixel np.array, greyscale.
+    bbox  - 2x2 np.array, bounding box, relative to left image, from top left
+            corner to bottom right corner (inclusive). (x, y) in columns.
+    maxd  - Integer, maximum disparity value; disparities must be within zero
+            to maxd inclusive (i.e., don't search beyond maxd).
+
+    Returns:
+    --------
+    Id  - Disparity image (map) as np.array, same size as Il.
+    """
+    # Hints:
+    #
+    #  - Loop over each image row, computing the local similarity measure, then
+    #    aggregate. At the border, you may replicate edge pixels, or just avoid
+    #    using values outside of the image.
+    #
+    #  - You may hard-code any parameters you require in this function.
+    #
+    #  - Use whatever window size you think might be suitable.
+    #
+    #  - Optimize for runtime AND for clarity.
+
+    #--- FILL ME IN ---
+
+    # Code goes here...
+
+    #------------------
+
+    correct = isinstance(Id, np.ndarray) and Id.shape == Il.shape
+
+    if not correct:
+        raise TypeError("Wrong type or size returned!")
+
+    return Id
